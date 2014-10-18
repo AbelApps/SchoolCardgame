ccfa8c54e35212a8120faa5758ee5b0560f08bab
AbelApps <biorealisme@gmail.com>
2014-10-19 00:34:34 +0200

    Game.html

---
+++ js/game.js
@@ -0,0 +1,13 @@
+var Startgame = function() {
+    var p = 4;
+    var cards = 32;
+    var player1;
+    var player2;
+    var player3;
+    var player4;
+    
+    for (i = 0; i < 32; i++) {
+        var chosen = Math.random * 4
+        document.write(chosen)
+    };
+};
