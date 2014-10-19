d95e24468de8b14f2bbb5ba2a910444b3c0df98d
AbelApps <biorealisme@gmail.com>
2014-10-19 20:41:39 +0200

    Test of alert

--- js/game.js
+++ js/game.js
@@ -1,18 +1,7 @@
-var Startgame = function() {
-    var player1 = [];
-    var player2 = [];
-    var player3 = [];
-    var player4 = [];
-    var cardpersuit = 8;
-    
-    (function(){
-        var player = Math.floor(Math.random() * 4)
-        player.toString;
-        alert(player);
-    })
-    
-};
-
-$( "#Startgame").click(function(){
-    alert("button clicked");
-};)
+jQuery(function(){
+    $("button").click(function(){
+        alert("Button clicked");
+    });
+});
+                    
+        
